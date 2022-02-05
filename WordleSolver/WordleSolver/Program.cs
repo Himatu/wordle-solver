046c6271f794dc998306830006fa5c6ad2c1571e
Himatu <himatu.coding@gmail.com>
2022-02-06 05:35:06 +0900

    プロジェクト追加、仮コード追加

---
+++ WordleSolver/WordleSolver/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+
+#region main
+// これもsolverに持たせたほうがいいかも
+string[] words = System.IO.File.ReadAllLines(@"D:\Personal Projects\wordle-solver\WordleSolver\WordleSolver\sgb-words.txt");
+PlayWordle(words);
+#endregion main
+
+void PlayWordle(string[] words)
+{
+    // 1. choose a random word, which is the answer
+    Random r = new Random();
+    int randomIdx = r.Next(0, words.Count());
+    string answerWord = words[randomIdx];
+    
+    // 2. Find the answer
+    WordleSolver solver = new WordleSolver();
+    (var solved, var numTrials) = solver.Solve(answerWord);
+}
+
+List<LetterInfo> GetLetterInfos(string input, string answer)
+{
+    // todo
+    return new List<LetterInfo>();
+}
+
+void GetAlphabetFrequencies(string[] words, out Dictionary<char, uint> alphbetFreqs)
+{
+    alphbetFreqs = new Dictionary<char, uint>(0);
+    {
+        alphbetFreqs.Add('a', 0);
+        alphbetFreqs.Add('b', 0);
+        alphbetFreqs.Add('c', 0);
+        alphbetFreqs.Add('d', 0);
+        alphbetFreqs.Add('e', 0);
+        alphbetFreqs.Add('f', 0);
+        alphbetFreqs.Add('g', 0);
+        alphbetFreqs.Add('h', 0);
+        alphbetFreqs.Add('i', 0);
+        alphbetFreqs.Add('j', 0);
+        alphbetFreqs.Add('k', 0);
+        alphbetFreqs.Add('l', 0);
+        alphbetFreqs.Add('m', 0);
+        alphbetFreqs.Add('n', 0);
+        alphbetFreqs.Add('o', 0);
+        alphbetFreqs.Add('p', 0);
+        alphbetFreqs.Add('q', 0);
+        alphbetFreqs.Add('r', 0);
+        alphbetFreqs.Add('s', 0);
+        alphbetFreqs.Add('t', 0);
+        alphbetFreqs.Add('u', 0);
+        alphbetFreqs.Add('v', 0);
+        alphbetFreqs.Add('w', 0);
+        alphbetFreqs.Add('x', 0);
+        alphbetFreqs.Add('y', 0);
+        alphbetFreqs.Add('z', 0);
+    }
+    foreach (string word in words)
+    {
+        foreach (char letter in word)
+        {
+            alphbetFreqs[letter]++;
+        }
+    }
+}
+
+/// <summary>
+/// Get a word wihch has the highest total of consisting letters' frequencies and occurs first.
+/// </summary>
+string GetWordWithMostFrequentLetters(string[] words, Dictionary<char, uint> alphbetFreqs)
+{
+    string returnWord = "";
+    uint curMaxScore = 0;
+    foreach (string word in words)
+    {
+        uint score = 0;
+        foreach (char letter in word)
+        {
+            score += alphbetFreqs[letter];
+        }
+        if (score > curMaxScore)
+        {
+            curMaxScore = score;
+            returnWord = word;
+        }
+    }
+    return returnWord;
+}
---
+++ WordleSolver/WordleSolver/WordleInfo.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+
+public class LetterInfo
+{
+    public LetterInfo()
+    {
+        letter = null;
+        status = Status.NotUsedAsInput;
+        possibleIndices = new List<byte>();
+    }
+
+    enum Status
+    {
+        NotUsedAsInput, // Gray     
+        NotInAnswerWord,// Black
+        InAnswerWord,   // Yellow
+        LocationKnown,  // Green
+    }
+    char? letter;
+    Status status;
+    List<byte> possibleIndices;
+}
+
+public class WordleSolver
+{
+    private static readonly byte NumAlphabets = 26;
+    private static readonly byte WordLength = 5;
+    private static readonly byte NumTrials = 6;
+
+    private List<LetterInfo> _letterInfos;
+    private string? _answerWord; 
+
+    public WordleSolver()
+    {
+        _letterInfos = new List<LetterInfo>(NumAlphabets);
+        _answerWord = null;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="answerWord">正解の単語</param>
+    /// <returns>(bool, byte) = (正解か, 試行回数)</returns>
+    public (bool, byte) Solve(string answerWord)
+    {
+        Debug.Assert(answerWord != null && answerWord.Count() == WordLength);
+        _answerWord = answerWord;
+
+        // time performance
+        Stopwatch watch = Stopwatch.StartNew();
+
+        // solve
+        bool answeredCorrectly = false;
+        byte curNumTrials = 0;
+        while (curNumTrials < NumTrials)
+        {
+            curNumTrials++;
+            var inputWord = GetBestInputWord();
+            Console.WriteLine($"Input {curNumTrials} : {inputWord}");
+            if (inputWord == _answerWord)
+            {
+                answeredCorrectly = true;
+                break;
+            }
+            else
+            {
+                AddLetterInfos(inputWord);
+            }
+        }
+
+        watch.Stop();
+        // Print the result
+        Console.WriteLine($"Answer  : {answerWord}");
+        Console.WriteLine($"Trials  : {curNumTrials}");
+        Console.WriteLine($"Time    : {watch.ElapsedMilliseconds} ms");
+
+        return (answeredCorrectly, curNumTrials);
+    }
+
+    public void AddLetterInfos(string inputWord)
+    {
+        // todo
+    }
+
+    public string GetBestInputWord()
+    {
+        // todo
+        return "";
+    }
+
+}
