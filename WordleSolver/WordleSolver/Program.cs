80b25ddb2be018ee5466beaf57a734f61ec38d9f
Himatu <himatu.coding@gmail.com>
2022-02-07 01:57:06 +0900

    単純なWordleハードモードソルバー実装

--- WordleSolver/WordleSolver/Program.cs
+++ WordleSolver/WordleSolver/Program.cs
@@ -1,88 +1,19 @@
 ﻿using System.Diagnostics;
 
-#region main
-// これもsolverに持たせたほうがいいかも
+const int N = 10000;
 string[] words = System.IO.File.ReadAllLines(@"D:\Personal Projects\wordle-solver\WordleSolver\WordleSolver\sgb-words.txt");
-PlayWordle(words);
-#endregion main
-
-void PlayWordle(string[] words)
+WordleSolver solver = new WordleSolver(words);
+var numSolved = 0;
+var totalNumTrialsOfSolvedQuestions = 0;
+for (int i = 0; i < N; i++)
 {
-    // 1. choose a random word, which is the answer
-    Random r = new Random();
-    int randomIdx = r.Next(0, words.Count());
-    string answerWord = words[randomIdx];
-    
-    // 2. Find the answer
-    WordleSolver solver = new WordleSolver();
-    (var solved, var numTrials) = solver.Solve(answerWord);
-}
-
-List<LetterInfo> GetLetterInfos(string input, string answer)
-{
-    // todo
-    return new List<LetterInfo>();
-}
-
-void GetAlphabetFrequencies(string[] words, out Dictionary<char, uint> alphbetFreqs)
-{
-    alphbetFreqs = new Dictionary<char, uint>(0);
+    // todo: パフォーマンスや構造改善、NormalMode対応、他のアルゴリズム探す
+    (var solved, var numTrials) = solver.SolveHardMode();
+    if (solved)
     {
-        alphbetFreqs.Add('a', 0);
-        alphbetFreqs.Add('b', 0);
-        alphbetFreqs.Add('c', 0);
-        alphbetFreqs.Add('d', 0);
-        alphbetFreqs.Add('e', 0);
-        alphbetFreqs.Add('f', 0);
-        alphbetFreqs.Add('g', 0);
-        alphbetFreqs.Add('h', 0);
-        alphbetFreqs.Add('i', 0);
-        alphbetFreqs.Add('j', 0);
-        alphbetFreqs.Add('k', 0);
-        alphbetFreqs.Add('l', 0);
-        alphbetFreqs.Add('m', 0);
-        alphbetFreqs.Add('n', 0);
-        alphbetFreqs.Add('o', 0);
-        alphbetFreqs.Add('p', 0);
-        alphbetFreqs.Add('q', 0);
-        alphbetFreqs.Add('r', 0);
-        alphbetFreqs.Add('s', 0);
-        alphbetFreqs.Add('t', 0);
-        alphbetFreqs.Add('u', 0);
-        alphbetFreqs.Add('v', 0);
-        alphbetFreqs.Add('w', 0);
-        alphbetFreqs.Add('x', 0);
-        alphbetFreqs.Add('y', 0);
-        alphbetFreqs.Add('z', 0);
-    }
-    foreach (string word in words)
-    {
-        foreach (char letter in word)
-        {
-            alphbetFreqs[letter]++;
-        }
+        numSolved++;
+        totalNumTrialsOfSolvedQuestions += numTrials;
     }
 }
-
-/// <summary>
-/// Get a word wihch has the highest total of consisting letters' frequencies and occurs first.
-/// </summary>
-string GetWordWithMostFrequentLetters(string[] words, Dictionary<char, uint> alphbetFreqs)
-{
-    string returnWord = "";
-    uint curMaxScore = 0;
-    foreach (string word in words)
-    {
-        uint score = 0;
-        foreach (char letter in word)
-        {
-            score += alphbetFreqs[letter];
-        }
-        if (score > curMaxScore)
-        {
-            curMaxScore = score;
-            returnWord = word;
-        }
-    }
-    return returnWord;
-}
+Console.WriteLine($"Success Rate : {(float)numSolved/N*100:0.00}%");
+Console.WriteLine($"Avg. Num Trials : {(float)totalNumTrialsOfSolvedQuestions/N}");
--- WordleSolver/WordleSolver/WordleSolver.cs
+++ WordleSolver/WordleSolver/WordleSolver.cs
@@ -1,38 +1,96 @@
 ﻿using System.Diagnostics;
 
-public class LetterInfo
+static class Extensions
 {
-    public LetterInfo()
+    public static int ToAlphabeticalIndex(this char c)
     {
-        letter = null;
-        status = Status.NotUsedAsInput;
-        possibleIndices = new List<byte>();
+        return char.ToLower(c) - 97;
     }
 
-    enum Status
+    public static char ToAlphabet(this int i)
     {
-        NotUsedAsInput, // Gray     
-        NotInAnswerWord,// Black
-        InAnswerWord,   // Yellow
-        LocationKnown,  // Green
+        return (char)(i + 97);
     }
-    char? letter;
-    Status status;
-    List<byte> possibleIndices;
+
+    public static List<int> AllIndicesOf(this string s, char c)
+    {
+        var foundIndices = new List<int>();
+        for (int indexOfC = s.IndexOf(c); indexOfC > -1; indexOfC = s.IndexOf(c, indexOfC + 1 /* startIndex */))
+        {
+            foundIndices.Add(indexOfC);
+        }
+        if(foundIndices.Count == 0)
+        {
+            foundIndices.Add(-1);
+        }
+        return foundIndices;
+    }
 }
 
 public class WordleSolver
 {
+    private class LetterInfo
+    {
+        public LetterInfo()
+        {
+            Status = StatusFlag.NotUsedAsInput;
+            WrongIndices = new HashSet<int>();
+            CorrectIndices = new HashSet<int>();
+        }
+
+        public void Reset()
+        {
+            Status = StatusFlag.NotUsedAsInput;
+            WrongIndices.Clear();
+            CorrectIndices.Clear();
+        }
+
+        public enum StatusFlag
+        {
+            NotUsedAsInput, // Gray     
+            NotInAnswerWord,// Black
+            InAnswerWord,   // Yellow
+            LocationKnown,  // Green
+        }
+        public StatusFlag Status;
+        public HashSet<int> WrongIndices;
+        public HashSet<int> CorrectIndices;
+    }
+
     private static readonly byte NumAlphabets = 26;
     private static readonly byte WordLength = 5;
     private static readonly byte NumTrials = 6;
 
-    private List<LetterInfo> _letterInfos;
-    private string? _answerWord; 
-
-    public WordleSolver()
+    // 
+    private string[] _wordPool;
+    public string[] WordPool
     {
-        _letterInfos = new List<LetterInfo>(NumAlphabets);
+        //get { return _wordPool; }
+        set
+        {
+            _wordPool = value;
+        }
+    }
+    private List<LetterInfo> _letterInfos;
+    private string? _answerWord;
+
+    public WordleSolver(string[] wordPool)
+    {
+        _wordPool = wordPool;
+        // todo: yellow/greenのアルファベット数まで削減し随時Addに変更
+        _letterInfos = new List<LetterInfo>(NumAlphabets)
+        {
+            new LetterInfo(), new LetterInfo(), new LetterInfo(),
+            new LetterInfo(), new LetterInfo(), new LetterInfo(),
+            new LetterInfo(), new LetterInfo(), new LetterInfo(),
+            new LetterInfo(), new LetterInfo(), new LetterInfo(),
+            new LetterInfo(), new LetterInfo(), new LetterInfo(),
+            new LetterInfo(), new LetterInfo(), new LetterInfo(),
+            new LetterInfo(), new LetterInfo(), 
+            new LetterInfo(), new LetterInfo(), 
+            new LetterInfo(), new LetterInfo(), 
+            new LetterInfo(), new LetterInfo(), 
+        };
         _answerWord = null;
     }
 
@@ -41,10 +99,9 @@ public class WordleSolver
     /// </summary>
     /// <param name="answerWord">正解の単語</param>
     /// <returns>(bool, byte) = (正解か, 試行回数)</returns>
-    public (bool, byte) Solve(string answerWord)
+    public (bool, byte) SolveHardMode()
     {
-        Debug.Assert(answerWord != null && answerWord.Count() == WordLength);
-        _answerWord = answerWord;
+        _answerWord = GetRandomWord();
 
         // time performance
         Stopwatch watch = Stopwatch.StartNew();
@@ -70,22 +127,227 @@ public class WordleSolver
 
         watch.Stop();
         // Print the result
-        Console.WriteLine($"Answer  : {answerWord}");
+        Console.WriteLine($"Answer  : {_answerWord}");
         Console.WriteLine($"Trials  : {curNumTrials}");
         Console.WriteLine($"Time    : {watch.ElapsedMilliseconds} ms");
-
+        // reset for the next one
+        Reset();
         return (answeredCorrectly, curNumTrials);
     }
 
-    public void AddLetterInfos(string inputWord)
+    private void Reset()
     {
-        // todo
+        foreach (var letterInfo in _letterInfos)
+        {
+            letterInfo.Reset();
+        }
     }
 
-    public string GetBestInputWord()
+    private string GetRandomWord()
     {
-        // todo
-        return "";
+        Random r = new Random();
+        int randomIdx = r.Next(0, _wordPool.Count());
+        return _wordPool[randomIdx];
+    }
+
+    private void AddLetterInfos(string inputWord)
+    {
+        for(int i = 0; i < WordLength; i++)
+        {
+            var letterInfo = _letterInfos[inputWord[i].ToAlphabeticalIndex()];
+            var answerCharIndices = _answerWord?.AllIndicesOf(inputWord[i]);
+            // ここら辺の処理整理したい
+            foreach (var answerCharIndex in answerCharIndices)
+            {
+                if (answerCharIndex == i)
+                {
+                    letterInfo.Status = LetterInfo.StatusFlag.LocationKnown;
+                    letterInfo.CorrectIndices.Add(i);
+                    if (letterInfo.WrongIndices.Contains(i))
+                    {
+                        letterInfo.WrongIndices.Remove(i);
+                    }
+                }
+                else if (answerCharIndex == -1)
+                {
+                    letterInfo.Status = LetterInfo.StatusFlag.NotInAnswerWord;
+                }
+                else
+                {
+                    if (letterInfo.Status != LetterInfo.StatusFlag.LocationKnown)
+                    {
+                        letterInfo.Status = LetterInfo.StatusFlag.InAnswerWord;
+                    }
+                    if (!letterInfo.CorrectIndices.Contains(i))
+                    {
+                        letterInfo.WrongIndices.Add(i);
+                    }
+                    // WordLength - 1箇所間違っている場所がわかっているなら、残り1か所が正解の場所と確定する.
+                    if (letterInfo.WrongIndices.Count() == WordLength - 1)
+                    {
+                        letterInfo.Status = LetterInfo.StatusFlag.LocationKnown;
+
+                        var sumFromOneToWordLength = (WordLength-1) * (WordLength) / 2;
+                        var sumWrongIndices = 0;
+                        foreach (var wrongIndex in letterInfo.WrongIndices)
+                        {
+                            sumWrongIndices += wrongIndex;
+                        }
+                        var correctIdx = sumFromOneToWordLength - sumWrongIndices;
+                        letterInfo.CorrectIndices.Add(correctIdx);
+                    }
+
+                }
+            }
+        }
+    }
+
+    private string GetBestInputWord()
+    {
+        List<string> candidateWords = new List<string>();
+        foreach (string word in _wordPool)
+        {
+            bool isCandidate = true;
+            for (int i = 0; i < _letterInfos.Count(); i++)
+            {
+                var letterInfo = _letterInfos[i];
+                switch (letterInfo.Status)
+                {
+                    case LetterInfo.StatusFlag.NotUsedAsInput:
+                    break;
+                    case LetterInfo.StatusFlag.NotInAnswerWord:
+                    {
+                        // AnswerWordに含まれていない黒のアルファベットが含まれている場合は候補から外れる。
+                        if (word.Contains(i.ToAlphabet()))
+                        {
+                            isCandidate = false;
+                        }
+                    }
+                    break;
+                    case LetterInfo.StatusFlag.InAnswerWord:
+                    {
+                        if (!word.Contains(i.ToAlphabet()))
+                        {
+                            isCandidate = false;
+                            break;
+                        }
+                        foreach (var wrongIdx in letterInfo.WrongIndices)
+                        {
+                            if (word[wrongIdx] == i.ToAlphabet())
+                            {
+                                isCandidate = false;
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                    case LetterInfo.StatusFlag.LocationKnown:
+                    {
+                        if (!word.Contains(i.ToAlphabet()))
+                        {
+                            isCandidate = false;
+                            break;
+                        }
+                        foreach (var correctIdx in letterInfo.CorrectIndices)
+                        {
+                            if (word[correctIdx] != i.ToAlphabet())
+                            {
+                                isCandidate = false;
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                    default:
+                    break;
+                }
+                if (!isCandidate)
+                {
+                    break;
+                }
+            }
+            if (isCandidate)
+            {
+                candidateWords.Add(word);
+            }
+        }
+        string returnWord = GetBestInputWordFromCandidates(candidateWords.ToArray());
+        Debug.Assert(returnWord.Count() == WordLength);
+        return returnWord;
+    }
+
+    private string GetBestInputWordFromCandidates(string[] candidateWords)
+    {
+        GetAlphabetFrequencies(candidateWords.ToArray(), out Dictionary<char, uint> alphbetFreqs);
+        return GetWordWithMostFrequentLetters(candidateWords.ToArray(), alphbetFreqs);
+    }
+
+    private void GetAlphabetFrequencies(string[] words, out Dictionary<char, uint> alphbetFreqs)
+    {
+        alphbetFreqs = new Dictionary<char, uint>(0);
+        {
+            alphbetFreqs.Add('a', 0);
+            alphbetFreqs.Add('b', 0);
+            alphbetFreqs.Add('c', 0);
+            alphbetFreqs.Add('d', 0);
+            alphbetFreqs.Add('e', 0);
+            alphbetFreqs.Add('f', 0);
+            alphbetFreqs.Add('g', 0);
+            alphbetFreqs.Add('h', 0);
+            alphbetFreqs.Add('i', 0);
+            alphbetFreqs.Add('j', 0);
+            alphbetFreqs.Add('k', 0);
+            alphbetFreqs.Add('l', 0);
+            alphbetFreqs.Add('m', 0);
+            alphbetFreqs.Add('n', 0);
+            alphbetFreqs.Add('o', 0);
+            alphbetFreqs.Add('p', 0);
+            alphbetFreqs.Add('q', 0);
+            alphbetFreqs.Add('r', 0);
+            alphbetFreqs.Add('s', 0);
+            alphbetFreqs.Add('t', 0);
+            alphbetFreqs.Add('u', 0);
+            alphbetFreqs.Add('v', 0);
+            alphbetFreqs.Add('w', 0);
+            alphbetFreqs.Add('x', 0);
+            alphbetFreqs.Add('y', 0);
+            alphbetFreqs.Add('z', 0);
+        }
+        foreach (string word in words)
+        {
+            foreach (char letter in word)
+            {
+                alphbetFreqs[letter]++;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Get a word wihch has the highest total of consisting letters' frequencies and occurs first.
+    /// </summary>
+    private string GetWordWithMostFrequentLetters(string[] words, Dictionary<char, uint> alphbetFreqs)
+    {
+        string returnWord = "";
+        uint curMaxScore = 0;
+        foreach (string word in words)
+        {
+            uint score = 0;
+            List<char> lettersAppeared = new List<char>();
+            foreach (char letter in word)
+            {
+                if (!lettersAppeared.Contains(letter))
+                {
+                    lettersAppeared.Add(letter);
+                    score += alphbetFreqs[letter];
+                }
+            }
+            if (score > curMaxScore)
+            {
+                curMaxScore = score;
+                returnWord = word;
+            }
+        }
+        return returnWord;
     }
 
 }
